105e786a5a7d406215edcd71a0fe346c9181fc07
dinh dat <dinhquocdat1310@gmail.com>
2024-04-16 13:45:02 +0700

    Add logout, get refresh token for auth, remove unused comment code

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,36 +1,35 @@
 import { CreateLoginAuthInput } from './dto/create-auth.input';
-import { Resolver, Mutation, Args, Int, Context ,Query} from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Int, Context, Query } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { GitHubCode } from './dto/auth';
-import { GitHubAuth } from './entities/auth.entity';
+import {
+  AuthAccessToken,
+  AuthMessage,
+  GitHubAuth,
+} from './entities/auth.entity';
 import { GqlLocalAuthGuard } from './guards/local-auth.guard';
 import { UseGuards } from '@nestjs/common';
 import { AuthToken } from './entities/auth.entity';
 import { StatusGuard } from 'src/guard/userStatus.guard';
 import { Status } from 'src/guard/decorators';
 import { StatusUser } from '@prisma/client';
+import { AccessJwtAuthGuard } from './guards/jwt-access-auth.guard';
+import { RefreshJwtAuthGuard } from './guards/jwt-refresh-auth.guard';
 
 @Resolver(() => GitHubAuth)
 export class AuthGitHubResolver {
   constructor(private readonly authService: AuthService) {}
-  
-  
-
-
-  // @Query(() => AuthToken)
-  // githubLogin(@Args('gitHubCode') gitHubCode: GitHubCode) {
-
-  //   return this.authService.githubLogin(gitHubCode);
-  // }
 }
 
 @Resolver()
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
+
   @Query(() => AuthToken)
   githubLogin(@Args('gitHubCode') gitHubCode: GitHubCode) {
     return this.authService.githubLogin(gitHubCode);
   }
+
   @Mutation(() => AuthToken)
   @UseGuards(GqlLocalAuthGuard, StatusGuard)
   @Status(StatusUser.INIT, StatusUser.VERIFIED)
@@ -40,5 +39,19 @@ export class AuthResolver {
     @Context() context: any,
   ) {
     return await this.authService.login(context.user);
+  }
+
+  @Mutation(() => AuthMessage)
+  @UseGuards(AccessJwtAuthGuard)
+  @Status(StatusUser.INIT, StatusUser.VERIFIED)
+  async logout(@Context() context: any) {
+    return await this.authService.logout(context.req.user);
+  }
+
+  @Mutation(() => AuthAccessToken)
+  @UseGuards(RefreshJwtAuthGuard)
+  @Status(StatusUser.INIT, StatusUser.VERIFIED)
+  async refreshToken(@Context() context: any) {
+    return await this.authService.refreshToken(context.req.user);
   }
 }
